3b304b5e610434b617a2236e6e43f4f05dee0cd9
sithuphyo <sithuphyo88@gmail.com>
2016-06-06 22:14:15 +0630

    Change login

--- app/src/main/java/com/ideapro/cms/view/AboutFragment.java
+++ app/src/main/java/com/ideapro/cms/view/AboutFragment.java
@@ -10,6 +10,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.ideapro.cms.R;
